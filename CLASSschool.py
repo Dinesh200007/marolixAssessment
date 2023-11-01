c0ecf2c4a340a22b14bbd60c53cf505eb9c17891
Dinesh200007 <146006659+Dinesh200007@users.noreply.github.com>
2023-11-01 13:10:34 +0530

    Add files via upload

---
+++ CLASSschool.py
@@ -0,0 +1,22 @@
+class school:
+    def __init__(self,school_name,class_rooms,teachers,library,students):
+        self.school_name = school_name
+        self.class_rooms = class_rooms
+        self.teachers = teachers
+        self.library = library
+        self.students = students
+school1 = school("sri chatanya",20,15,1,300)
+school2 = school("vijaya bharathi",30,20,2,600)
+school3 = school("aravinda",25,18,1,450)
+school4 = school("ZPHS",35,28,2,800)
+school5 = school("goutham model school",25,17,1,400)
+school6 = school("vijaya high school",15,10,1,200)
+school7 = school("kids joy",33,22,1,350)
+school8 = school("george high",22,15,1,290)
+school9 = school("arunodhaya high school",25,19,1,300)
+school10 = school("teja high school",24,17,1,300)
+print(school1.class_rooms,school1.students)
+print(school2.school_name)
+print(school4.students)
+
+
